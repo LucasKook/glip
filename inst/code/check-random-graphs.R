6a6c5ed2329e1718c71489e0ca09d177bcc187d2
Lucas Kook <lucasheinrich.kook@gmail.com>
2025-07-09 12:17:40 +0200

    nop

--- inst/code/check-random-graphs.R
+++ inst/code/check-random-graphs.R
@@ -13,7 +13,7 @@ d <- darg(args[1], 3)
 max_size <- darg(args[2], d - 2)
 pp <- darg(args[3], 0.5)
 mode <- c("dag", "chain", "admg", "dagdcon")[darg(args[4], 1)]
-seeds <- eval(parse(text = darg(args[5], "1:100")))
+seeds <- 1:1000
 V <- letters[1:d]
 cache <- TRUE
 ncores <- max(7, parallel::detectCores(logical = TRUE) - 2)
