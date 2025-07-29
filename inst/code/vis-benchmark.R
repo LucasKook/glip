d9b561849823a1a8bd560ee701e3ea4ac318f617
Lucas Kook <lucasheinrich.kook@gmail.com>
2025-07-29 10:04:05 +0200

    vis

--- inst/code/vis-benchmark.R
+++ inst/code/vis-benchmark.R
@@ -2,9 +2,14 @@
 ### LK 2025
 
 library("tidyverse")
+save <- TRUE
 
 ### List files
-fin <- "./inst/results/benchmark/2025-07-28"
+fin <- "./inst/results/benchmark/2025-07-29/oracle"
+fout <- str_replace(fin, "results", "figures")
+if (!dir.exists(fout)) {
+  dir.create(fout, recursive = TRUE)
+}
 files <- list.files(fin, pattern = "*.rds", full.names = TRUE)
 
 ### Read files
@@ -18,13 +23,13 @@ timings <- res |>
   mutate(time = as.numeric(time)) |>
   summarize(q50 = median(time), q25 = quantile(time, 0.25), q75 = quantile(time, 0.75))
 
-ggplot(timings, aes(x = ordered(d), y = q50, color = method, ymin = q25, ymax = q75)) +
-  geom_point(position = position_dodge(0.3)) +
-  geom_linerange(position = position_dodge(0.3)) +
+p1 <- ggplot(timings, aes(x = ordered(d), y = q50, color = method, ymin = q25, ymax = q75)) +
+  geom_pointrange(position = position_dodge(0.8)) +
   facet_grid(~mode, labeller = as_labeller(c("dag" = "DAG", "admg" = "ADMG"))) +
   theme_bw() +
   labs(y = "median runtime in seconds", x = "number of nodes") +
-  scale_y_log10()
+  scale_y_log10() +
+  theme(text = element_text(size = 13.5), legend.position = "top")
 
 ### Performance
 pdat <- res |>
@@ -44,11 +49,21 @@ lbs <- c(
   "head_rec" = "1 - Recall (head)",
   "head_f1" = "1 - F1 (head)"
 )
-ggplot(
+
+p2 <- ggplot(
   pdat |> mutate(metric = factor(metric, levels = names(lbs))),
   aes(x = ordered(d), y = value, color = method)
 ) +
   stat_summary(position = position_dodge(0.8), fun.data = "mean_se") +
   facet_grid(mode ~ metric, labeller = as_labeller(lbs)) +
   theme_bw() +
-  labs(y = "score", x = "number of nodes")
+  labs(y = "score", x = "number of nodes") +
+  theme(text = element_text(size = 13.5), legend.position = "top")
+
+print(p1)
+print(p2)
+
+if (save) {
+  ggsave(file.path(fout, "timings.pdf"), p1, height = 4, width = 6)
+  ggsave(file.path(fout, "performance.pdf"), p2, height = 6, width = 18)
+}
