63ec045051c37af09db3ec75360e1bacfef9952c
Lucas Kook <lucasheinrich.kook@gmail.com>
2025-08-05 09:37:01 +0200

    change to nsim

--- inst/code/run-benchmark.R
+++ inst/code/run-benchmark.R
@@ -20,7 +20,7 @@ ms <- as.numeric(darg(args[3], d - 2))
 ms <- ifelse(ms == -1, d - 2, ms)
 degree <- as.numeric(darg(args[4], 2))
 n <- as.numeric(darg(args[5], 1e3))
-seed <- as.numeric(darg(args[6], 12))
+nsim <- as.numeric(darg(args[6], 1))
 alpha <- as.numeric(darg(args[7], 0.01))
 use_oracle_tests <- as.numeric(darg(args[8], 0))
 sim_name <- darg(args[9], "test-run")
@@ -35,153 +35,156 @@ cache <- TRUE
 # Parameters for running the tests
 targs <- list(reg_YonZ = "lrm", reg_XonZ = "lrm")
 
-# Output file
-outdir <- file.path(
-  "inst", "results", "benchmark", Sys.Date(), sim_name
-)
-fout <- paste0(
-  "res-mode_", mode, "-d_", d, "-ms_", ms, "-degree_",
-  degree, "-n_", n, "-seed_", seed, "-alpha_", alpha,
-  "-oracle_", use_oracle_tests, ".rds"
-)
-if (!dir.exists(outdir)) {
-  dir.create(outdir, recursive = TRUE)
-}
-
-### Generate random graph and data
-cat("\nGenerating random graph and data\n")
-set.seed(tseed <- 1e4 + 3e4 * (mode == "dag") + n + seed)
-graph <- random_graph(d = d, prob = pr, mode = mode)
-data <- data.frame(py_data <- scale(rgraph(graph, n = n)))
-py_data <- r_to_py(py_data)$copy()
-V <- colnames(data)
-
-### ORACLE
-gt <- switch(mode,
-  "dag" = graph$DAG,
-  "admg" = graph$ADMG
-)
-ORACLE <- .compute_graphical_representation(gt, ms, mode)
-
-### Run CITs
-cat("\nRunning conditional independence tests\n")
-tests <- otests <- .compute_oracle_tests(gt, ms, mode)
-if (!use_oracle_tests) {
-  tests <- learn_graph(
-    data = data, max_size = ms, mode = mode, test_args = targs,
-    return_tests_only = TRUE
+out <- lapply(seq_len(nsim), \(seed) {
+  # Output file
+  outdir <- file.path(
+    "inst", "results", "benchmark", Sys.Date(), sim_name
   )
-}
-input_sep <- mean(otests$p.value != 1 * (tests$p.value > alpha))
-
-### PC ALG (only under causal sufficiency/DAG case)
-cat("\nRunning PC\n")
-tstart <- Sys.time()
-pcres <- pcalg::pc(list(tests = tests, V = V), lookup_ci, labels = V, alpha = alpha)
-tstop <- Sys.time()
-runtime_PC <- tstop - tstart
-pcout <- as(pcres@graph, "matrix")
-PC <- pcout
-
-### FCI ALG
-cat("\nRunning FCI\n")
-tstart <- Sys.time()
-fcires <- pcalg::fciPlus(list(tests = tests, V = V), lookup_ci,
-  labels = V, alpha = alpha, selectionBias = FALSE, verbose = FALSE
-)
-tstop <- Sys.time()
-runtime_FCI <- tstop - tstart
-fciout <- as(fcires@amat, "matrix")
-FCI <- fciout
-
-### R2sortability
-cat("\nRunning R2SORT\n")
-tstart <- Sys.time()
-r2s <- 1 * (cd$r2_sort_regress(py_data) != 0)
-tstop <- Sys.time()
-runtime_R2SORT <- tstop - tstart
-dimnames(r2s) <- list(V, V)
-R2SORT <- .compute_graphical_representation(r2s, ms, mode)
-
-### GLIP
-cat("\nRunning GLIP\n")
-lG <- .get_opt(mode)(tests,
-  d = d, max_size = ms,
-  V = V, cache = cache,
-  trafo = \(x) as.numeric(x <= alpha),
-  weight_type = wtype,
-  warmstart = NULL,
-  edgehints = 1 * (R2SORT != 0),
-  gurobi_args = list(
-    Threads = ncores,
-    TimeLimit = walltime
-  ), mode = mode
-)
-
-GLIP <- .compute_graphical_representation(lG$graph, ms, mode)
-runtime_GLIP <- as.difftime(lG$optim$runtime, units = "secs")
-
-### NOTEARS
-cat("\nRunning NOTEARS\n")
-model <- dagma$DagmaLinear(loss_type = "l2")
-tstart <- Sys.time()
-nto <- 1 * (model$fit(py_data, lambda1 = 0.02) != 0)
-tstop <- Sys.time()
-runtime_NOTEARS <- tstop - tstart
-dimnames(nto) <- list(V, V)
-NOTEARS <- .compute_graphical_representation(nto, ms, mode)
-
-### Evaluate and summarize results
-outputs <- list(
-  GLIP = GLIP,
-  PC = PC,
-  FCI = FCI,
-  NOTEARS = NOTEARS,
-  R2SORT = R2SORT
-)
-timings <- list(
-  GLIP = runtime_GLIP,
-  PC = runtime_PC,
-  FCI = runtime_FCI,
-  NOTEARS = runtime_NOTEARS,
-  R2SORT = runtime_R2SORT
-)
-
-if (mode == "admg") { # remove PC in case of ADMGs
-  outputs <- outputs[-grep("PC", names(outputs))]
-  timings <- timings[-grep("PC", names(timings))]
-}
-
-cat("\nEvaluating and summarizing results\n")
-res <- lapply(seq_along(outputs), \(idx) {
-  method <- names(outputs)[[idx]]
-  learned <- outputs[[idx]]
-  SHD <- shd(learned, ORACLE)
-  if (method != "GLIP") {
-    SEP <- sep(learned, ORACLE, ifelse(mode == "dag", "pdag", "mag"), ms, oracle = otests)
-  } else {
-    SEP <- mean(lG$tests$dcon != 1 * (otests$p.value < alpha))
+  fout <- paste0(
+    "res-mode_", mode, "-d_", d, "-ms_", ms, "-degree_",
+    degree, "-n_", n, "-seed_", seed, "-alpha_", alpha,
+    "-oracle_", use_oracle_tests, ".rds"
+  )
+  if (!dir.exists(outdir)) {
+    dir.create(outdir, recursive = TRUE)
   }
-  CM <- prf1(learned, ORACLE)
-  data.frame(
-    method = method,
-    shd = SHD,
-    sep = SEP,
-    input_sep = input_sep,
-    tail_prec = 1 - mean(CM$precision[CM$which == "tail"], na.rm = TRUE),
-    tail_rec = 1 - mean(CM$recall[CM$which == "tail"], na.rm = TRUE),
-    tail_f1 = 1 - mean(CM$f1[CM$which == "tail"], na.rm = TRUE),
-    head_prec = 1 - mean(CM$precision[CM$which == "head"], na.rm = TRUE),
-    head_rec = 1 - mean(CM$recall[CM$which == "head"], na.rm = TRUE),
-    head_f1 = 1 - mean(CM$f1[CM$which == "head"], na.rm = TRUE),
-    time = as.difftime(timings[[idx]], units = "secs"),
-    d = d, ms = ms, n = n, degree = degree, mode = mode,
-    use_oracle_tests = use_oracle_tests
-  )
-}) |> do.call("rbind", args = _)
-res
 
-if (save) {
-  cat("\nSaving results\n")
-  saveRDS(res, file.path(outdir, fout))
-}
+  ### Generate random graph and data
+  cat("\nGenerating random graph and data\n")
+  set.seed(tseed <- 1e4 + 3e4 * (mode == "dag") + n + seed)
+  graph <- random_graph(d = d, prob = pr, mode = mode)
+  data <- data.frame(py_data <- scale(rgraph(graph, n = n)))
+  py_data <- r_to_py(py_data)$copy()
+  V <- colnames(data)
+
+  ### ORACLE
+  gt <- switch(mode,
+    "dag" = graph$DAG,
+    "admg" = graph$ADMG
+  )
+  ORACLE <- .compute_graphical_representation(gt, ms, mode)
+
+  ### Run CITs
+  cat("\nRunning conditional independence tests\n")
+  tests <- otests <- .compute_oracle_tests(gt, ms, mode)
+  if (!use_oracle_tests) {
+    tests <- learn_graph(
+      data = data, max_size = ms, mode = mode, test_args = targs,
+      return_tests_only = TRUE
+    )
+  }
+  input_sep <- mean(otests$p.value != 1 * (tests$p.value > alpha))
+
+  ### PC ALG (only under causal sufficiency/DAG case)
+  cat("\nRunning PC\n")
+  tstart <- Sys.time()
+  pcres <- pcalg::pc(list(tests = tests, V = V), lookup_ci, labels = V, alpha = alpha)
+  tstop <- Sys.time()
+  runtime_PC <- tstop - tstart
+  pcout <- as(pcres@graph, "matrix")
+  PC <- pcout
+
+  ### FCI ALG
+  cat("\nRunning FCI\n")
+  tstart <- Sys.time()
+  fcires <- pcalg::fciPlus(list(tests = tests, V = V), lookup_ci,
+    labels = V, alpha = alpha, selectionBias = FALSE, verbose = FALSE
+  )
+  tstop <- Sys.time()
+  runtime_FCI <- tstop - tstart
+  fciout <- as(fcires@amat, "matrix")
+  FCI <- fciout
+
+  ### R2sortability
+  cat("\nRunning R2SORT\n")
+  tstart <- Sys.time()
+  r2s <- 1 * (cd$r2_sort_regress(py_data) != 0)
+  tstop <- Sys.time()
+  runtime_R2SORT <- tstop - tstart
+  dimnames(r2s) <- list(V, V)
+  R2SORT <- .compute_graphical_representation(r2s, ms, mode)
+
+  ### GLIP
+  cat("\nRunning GLIP\n")
+  lG <- .get_opt(mode)(tests,
+    d = d, max_size = ms,
+    V = V, cache = cache,
+    trafo = \(x) as.numeric(x <= alpha),
+    weight_type = wtype,
+    warmstart = NULL,
+    edgehints = 1 * (R2SORT != 0),
+    gurobi_args = list(
+      Threads = ncores,
+      TimeLimit = walltime
+    ), mode = mode
+  )
+
+  GLIP <- .compute_graphical_representation(lG$graph, ms, mode)
+  runtime_GLIP <- as.difftime(lG$optim$runtime, units = "secs")
+
+  ### NOTEARS
+  cat("\nRunning NOTEARS\n")
+  model <- dagma$DagmaLinear(loss_type = "l2")
+  tstart <- Sys.time()
+  nto <- 1 * (model$fit(py_data, lambda1 = 0.02) != 0)
+  tstop <- Sys.time()
+  runtime_NOTEARS <- tstop - tstart
+  dimnames(nto) <- list(V, V)
+  NOTEARS <- .compute_graphical_representation(nto, ms, mode)
+
+  ### Evaluate and summarize results
+  outputs <- list(
+    GLIP = GLIP,
+    PC = PC,
+    FCI = FCI,
+    NOTEARS = NOTEARS,
+    R2SORT = R2SORT
+  )
+  timings <- list(
+    GLIP = runtime_GLIP,
+    PC = runtime_PC,
+    FCI = runtime_FCI,
+    NOTEARS = runtime_NOTEARS,
+    R2SORT = runtime_R2SORT
+  )
+
+  if (mode == "admg") { # remove PC in case of ADMGs
+    outputs <- outputs[-grep("PC", names(outputs))]
+    timings <- timings[-grep("PC", names(timings))]
+  }
+
+  cat("\nEvaluating and summarizing results\n")
+  res <- lapply(seq_along(outputs), \(idx) {
+    method <- names(outputs)[[idx]]
+    learned <- outputs[[idx]]
+    SHD <- shd(learned, ORACLE)
+    if (method != "GLIP") {
+      SEP <- sep(learned, ORACLE, ifelse(mode == "dag", "pdag", "mag"), ms, oracle = otests)
+    } else {
+      SEP <- mean(lG$tests$dcon != 1 * (otests$p.value < alpha))
+    }
+    CM <- prf1(learned, ORACLE)
+    data.frame(
+      method = method,
+      shd = SHD,
+      sep = SEP,
+      input_sep = input_sep,
+      tail_prec = 1 - mean(CM$precision[CM$which == "tail"], na.rm = TRUE),
+      tail_rec = 1 - mean(CM$recall[CM$which == "tail"], na.rm = TRUE),
+      tail_f1 = 1 - mean(CM$f1[CM$which == "tail"], na.rm = TRUE),
+      head_prec = 1 - mean(CM$precision[CM$which == "head"], na.rm = TRUE),
+      head_rec = 1 - mean(CM$recall[CM$which == "head"], na.rm = TRUE),
+      head_f1 = 1 - mean(CM$f1[CM$which == "head"], na.rm = TRUE),
+      time = as.difftime(timings[[idx]], units = "secs"),
+      d = d, ms = ms, n = n, degree = degree, mode = mode, wtype = wtype,
+      use_oracle_tests = use_oracle_tests, iter = seed, seed = tseed
+    )
+  }) |> do.call("rbind", args = _)
+
+  if (save) {
+    cat("\nSaving results\n")
+    saveRDS(res, file.path(outdir, fout))
+  }
+  res
+})
+out
