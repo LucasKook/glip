3c432987beda399d1361e8bf64f6c85e0d1aceeb
Lucas Kook <lucasheinrich.kook@gmail.com>
2025-08-21 16:08:15 +0200

    defaults

--- inst/code/run-asp-comparison.R
+++ inst/code/run-asp-comparison.R
@@ -12,7 +12,7 @@ library("tidyverse")
 
 ### Parse command line arguments
 args <- commandArgs(trailingOnly = TRUE)
-mode <- darg(args[1], "dmg")
+mode <- darg(args[1], "admg")
 d <- as.numeric(darg(args[2], 5))
 ms <- as.numeric(darg(args[3], -1))
 ms <- ifelse(ms == -1, d - 2, ms)
