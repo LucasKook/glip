126a4ea365572e721c89a46b3867431d6e340a49
Lucas Kook <lucasheinrich.kook@gmail.com>
2026-01-16 13:02:02 +0100

    restrict to larger graphs and save

--- inst/code/vis-asp-comparison.R
+++ inst/code/vis-asp-comparison.R
@@ -8,7 +8,7 @@ max_time <- 600
 walltime <- 600
 
 ### List files
-fin <- "./inst/results/asp-comparison/full"
+fin <- "./inst/results/asp-comparison/weak-cubic"
 nrow <- c(1, 2)[1 + str_detect(fin, "weak")]
 nm <- c("full", "weak")[1 + str_detect(fin, "weak")]
 fout <- str_replace(fin, "results", "figures")
@@ -43,6 +43,19 @@ p1 <- ggplot(timings, aes(x = time, linetype = method, color = ordered(d))) +
   scale_color_viridis_d()
 p1
 
+p3 <- ggplot(timings |> filter(d >= 6), aes(x = time, linetype = method, color = ordered(d))) +
+  stat_ecdf(pad = FALSE) +
+  theme_bw() +
+  facet_wrap(~mode, labeller = label_both) +
+  labs(x = "runtime in seconds", y = "relative rank", color = "d") +
+  scale_x_continuous(trans = "log10", labels = trans_format("log10", math_format(10^.x))) +
+  theme(text = element_text(size = 13.5), legend.position = "top") +
+  guides(color = guide_legend(nrow = nrow)) +
+  coord_flip(xlim = c(min(timings$time) * 0.99, max_time)) +
+  geom_vline(xintercept = walltime, linetype = 3, color = "darkred", size = 1) +
+  scale_color_viridis_d()
+p3
+
 rel_time <- res |> mutate(rel = as.numeric(glip / ifelse(is.infinite(asp), walltime, asp)))
 p2 <- ggplot(rel_time, aes(x = rel, color = ordered(d))) +
   geom_vline(xintercept = 1, linetype = 3, color = "darkred", size = 1) +
@@ -65,7 +78,30 @@ p2 <- ggplot(rel_time, aes(x = rel, color = ordered(d))) +
   )
 p2
 
+p4 <- ggplot(rel_time |> filter(d >= 6), aes(x = rel, color = ordered(d))) +
+  geom_vline(xintercept = 1, linetype = 3, color = "darkred", size = 1) +
+  stat_ecdf(pad = FALSE) +
+  theme_bw() +
+  facet_wrap(~mode, labeller = label_both) +
+  labs(x = "relative runtime GLIP/ASP", y = "relative rank", color = "d") +
+  scale_x_continuous(trans = "log10", labels = trans_format("log10", math_format(10^.x))) +
+  theme(text = element_text(size = 13.5), legend.position = "top") +
+  guides(color = guide_legend(nrow = 1)) +
+  scale_color_brewer(palette = "Dark2", labels = c("asp" = "ASP", "glip" = "GLIP")) +
+  coord_flip() +
+  scale_color_viridis_d() +
+  annotate(x = 100, y = 0.5, label = "ASP faster", size = 4, color = "gray", geom = "text") +
+  annotate(x = 1 / 100, y = 0.5, label = "GLIP faster", size = 4, color = "gray", geom = "text") +
+  theme(
+    panel.background = element_rect(fill = "transparent", color = NA),
+    plot.background = element_rect(fill = "transparent", color = NA),
+    legend.background = element_rect(fill = "transparent", color = NA)
+  )
+p4
+
 if (save) {
   ggsave(file.path(fout, paste0("timings-", nm, ".pdf")), p1, height = 4.5, width = 7.5)
   ggsave(file.path(fout, paste0("rel-timings-", nm, ".pdf")), p2, height = 4.5, width = 7.5)
+  ggsave(file.path(fout, paste0("timings-", nm, "-large-d.pdf")), p4, height = 4.5, width = 7.5)
+  ggsave(file.path(fout, paste0("rel-timings-", nm, "-large-d.pdf")), p4, height = 4.5, width = 7.5)
 }
